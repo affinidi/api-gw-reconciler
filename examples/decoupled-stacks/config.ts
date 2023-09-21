5ef629a8b249b53d62c33b6cef9fb943ee480aed
Sanjay <sanjayshekhar@live.com>
2023-09-21 19:00:11 +0530

    Update config.ts

--- examples/decoupled-stacks/config.ts
+++ examples/decoupled-stacks/config.ts
@@ -1,3 +1,3 @@
-export const reconcilerMergedBucketArn="arn:aws:s3:::standalonereoncilerstack-reconcilermergedopenapid-1dl64grbno42y";
-export const reconcilerAPISchemaBucketArn= "arn:aws:s3:::standalonereoncilerstack-reconcileropenapidefinit-1drpnhtht049n";
-export const centralAPIGW="arn:aws:execute-api:ap-south-1:912469213006:1shn4k37a6/*/*/"
+export const reconcilerMergedBucketArn="arn:aws:s3:::sxxxxxxx";
+export const reconcilerAPISchemaBucketArn= "arn:aws:s3:::xxxx";
+export const centralAPIGW="arn:aws:execute-api:ap-south-1:cccccvvv"
